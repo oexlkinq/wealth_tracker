ca8359e00156e6315e47b383e69fea04bad21229
yyko <68428103+oexlkinq@users.noreply.github.com>
2025-11-21 21:55:43 +0500

    wip 2

---
+++ cmd/add/balance/balance.go
@@ -0,0 +1,11 @@
+package balance
+
+import "github.com/spf13/cobra"
+
+var BalanceCmd = &cobra.Command{
+	Use:   "balance",
+	Short: "Add new balance",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
